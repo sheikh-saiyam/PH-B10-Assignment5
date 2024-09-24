427f09ed800fce9e640bf5f1dbda5c58f4fa64ca
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2024-09-24 17:31:57 +0600

    button color active

--- js/index.js
+++ js/index.js
@@ -8,77 +8,22 @@ document.getElementById('history-btn').addEventListener('click', function(){
     document.getElementById('history-box').classList.remove('hidden')
 })
 
+const donationPageBtn = document.getElementById('donate-btn');
+const historyPageBtn = document.getElementById('history-btn');
 
-// const donationPageBtn = document.getElementById('donate-btn');
-// const historyPageBtn = document.getElementById('history-btn');
+historyPageBtn.addEventListener("click", function(){
 
-// historyPageBtn.addEventListener("click", function(){
-//     document.getElementById('donation-box').classList.add('hidden')
-//     document.getElementById('history-box').classList.remove('hidden')
+    historyPageBtn.classList.add('bg-[#B4F461]');
+    // historyPageBtn.classList.remove('');
 
-//     historyPageBtn.classList.add('bg-[#B4F461]');
-//     historyPageBtn.classList.remove('bg-slate-100 ');
+    donationPageBtn.classList.remove('bg-[#B4F461]');
+})
 
-//     donationPageBtn.classList.remove('bg-[#B4F461]');
-//     donationPageBtn.classList.add('bg-slate-100 ');
-// })
+donationPageBtn.addEventListener("click", function(){
 
-// donationPageBtn.addEventListener("click", function(){
-//     document.getElementById('donation-box').classList.remove('hidden')
-//     document.getElementById('history-box').classList.add('hidden')
-//     historyPageBtn.classList.remove('bg-[#B4F461]');
-//     historyPageBtn.classList.add('bg-slate-100 ');
+    historyPageBtn.classList.remove('bg-[#B4F461]');
+    // historyPageBtn.classList.add('bg-slate-100 ');
 
-//     donationPageBtn.classList.add('bg-[#B4F461]');
-//     donationPageBtn.classList.remove('bg-slate-100');
-// })
-
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   // Get the current page URL
-//   const currentPage = window.location.pathname;
-
-//   // Get the buttons
-//   const donationButton = document.getElementById("donation-btn");
-//   const historyButton = document.getElementById("history-btn");
-
-//   // checking btn before applying styles
-//   if (donationButton && historyButton) {
-//     // Check which page is active and change button colors
-//     if (currentPage.includes("index.html")) {
-//       // Set the colors for button
-//       donationButton.style.backgroundColor = "#B4F461";
-//       historyButton.style.backgroundColor = "";
-//     } else if (currentPage.includes("history.html")) {
-//       historyButton.style.backgroundColor = "#B4F461";
-//       donationButton.style.backgroundColor = "";
-//     }
-//   }
-// });
-
-
-
-// const historyTab = document.getElementById('history-btn');
-// const donationTab = document.getElementById("donation-btn");
-// historyTab.addEventListener('click', function(){
-
-//     historyTab.classList.add( 'bg-yellow-200');
-
-
-//     donationTab.classList.remove('bg-yellow-200');
-
-//     document.getElementById('donation-box').classList.add('hidden');
-
-//     document.getElementById("history-box").classList.remove("hidden");
-// })
-
-// donationTab.addEventListener('click', function(){
-//     donationTab.classList.add('bg-yellow-200')
-
-//     historyTab.classList.remove('bg-yellow-200');
-
-//     // document.getElementById('donation-box').classList.remove('hidden');
-
-//     // document.getElementById("history-box").classList.add("hidden");
-
-// })
+    donationPageBtn.classList.add('bg-[#B4F461]');
+    
+})
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// for no1 donation box 
 document.getElementById('donation-btn').addEventListener('click', function(e){
     e.preventDefault();
     const donateInput = parseFloat(document.getElementById('donate-input').value);
@@ -38,10 +39,61 @@ document.getElementById('donation-btn').addEventListener('click', function(e){
     
     historyItem.innerHTML= `
     <p class="font-bold px-3 py-2 "> ${donateInput} Taka is donated for ${donationSubject}</p>
-    <p class="text-pText px-3 "> Date: ${new Date().toLocaleString()} (Bangladesh Standard Time)</p>`;
+    <p class="text-pText px-3 "> Date: ${new Date()}</p>`;
 
     const historyContainer = document.getElementById("history-box");
     historyContainer.insertBefore(historyItem, historyContainer.firstChild);
+    // history section code 
+    }
+
+})
+
+
+// for no2 donation box 
+document.getElementById('donation-btn2').addEventListener('click', function(e){
+    e.preventDefault();
+    const donateInput2 = parseFloat(document.getElementById('donate-input2').value);
+    const donateBalance2 = document.getElementById('donate-balance2');
+    const coinDonateAmount = document.getElementById('coin-donate-amount');
+
+    if(donateInput2 <= 0 ){
+        alert('Please enter an amount greater than zero.');
+        document.getElementById('my_modal_1').close();
+    }
+
+    else if (isNaN(donateInput2)){
+        alert('Invalid Donation Amount');
+        document.getElementById('my_modal_1').close();
+    }
+
+    else if (coinDonateAmount.innerText < donateInput2) {
+        alert('donation amount is greater than the account balance');
+        document.getElementById('my_modal_1').close();
+    }
+    
+    else {
+    // input section code
+    let floatDonateBalance2 = parseFloat(donateBalance2.innerText);
+    let totalDonate2 =  donateInput2 + floatDonateBalance2 ;
+    donateBalance2.innerText = totalDonate2;
+
+    let floatCoinDonateAmount = parseFloat(coinDonateAmount.innerText);
+    let afterTotalDonate = floatCoinDonateAmount -  donateInput2 ;
+    coinDonateAmount.innerText = afterTotalDonate;
+    // input section code
+
+    // history section code 
+    const donationSubject2 = document.getElementById('donation-subject2').innerText;
+    const historyItem2 = document.createElement("div");
+
+    historyItem2.className ="bg-white rounded-lg border border-[#e8e8e8] p-6 border-2 shadow-sm ";
+    
+    historyItem2.innerHTML= `
+    <p class="font-bold px-3 py-2 "> ${donateInput2} Taka is donated for ${donationSubject2}</p>
+    <p class="text-pText px-3 "> Date: ${Date().toLocaleString()}</p>`;
+
+    const historyContainer2 = document.getElementById("history-box");
+    historyContainer2.insertBefore(historyItem2, historyContainer2.firstChild);
     // history section code 
     }
 
