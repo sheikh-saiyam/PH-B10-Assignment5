2d1b026eb1a88f17d0916424dd329dd5803ebb07
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2024-09-25 10:17:12 +0600

    all done

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 document.getElementById('donate-btn').addEventListener('click', function(){
     document.getElementById('donation-box').classList.remove('hidden')
     document.getElementById('history-box').classList.add('hidden')
+    document.getElementById('footer').classList.remove('hidden')
 })
 
 document.getElementById('history-btn').addEventListener('click', function(){
     document.getElementById('donation-box').classList.add('hidden')
     document.getElementById('history-box').classList.remove('hidden')
+    document.getElementById('footer').classList.add('hidden')
 })
 
 const donationPageBtn = document.getElementById('donate-btn');
